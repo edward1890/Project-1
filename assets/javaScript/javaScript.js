653fe2ab8b44200700ce1cae1e447b16cb1c9812
unknown <ejzagala@gmail.com>
2018-03-23 18:00:40 -0600

    Fixed firebase push with logic for undefined values, created firebase child IDs, captured lat/long values, pushing search results into distinct firebase children, cleaned up html

--- assets/javaScript/javaScript.js
+++ assets/javaScript/javaScript.js
@@ -1,41 +1,114 @@
 // Renamed javaScript from java 
 
-$(".btn-primary").on("click", function(event){
-    event.preventDefault();  
+//Declare firebase obj. 
+var config = {
+    apiKey: "AIzaSyB2w2Z0XxrOtqNiuXnheoco3l0Rq2_1Bhc",
+    authDomain: "localshow-befb6.firebaseapp.com",
+    databaseURL: "https://localshow-befb6.firebaseio.com",
+    projectId: "localshow-befb6",
+    storageBucket: "",
+    messagingSenderId: "727534673587"
+  };
+
+//Initialize firebase DB 
+firebase.initializeApp(config);
+
+//Call this magic database method
+var database = firebase.database(); 
+
+var searchResultID = 0; 
+
+//Click event for the main button on the landing page 
+$("#search-button").on("click", function(event){
+
+    searchResultID++; 
 
     //Capture input string 
     var keyword = $("#search-text").val().trim(); 
 
-    //Pass that string as the keyword into the query url
+    //Pass that string as the keyword into the query url. We're also using a proxy to side step CORS error. This will be removed when we go live. 
     var queryURL = "https://thingproxy.freeboard.io/fetch/" + "https://app.ticketmaster.com/discovery/v2/events.json?apikey=RoDgdYM6hvCYQYDMGjOgTU0jJBvdaXIg&city=denver&stateCode=CO&radius=50&keyword=" + keyword;
 
+    //Make the ajax call
     $.ajax({
         method: "GET", 
         url: queryURL
     }).then(function(response){
-        
-       var events = response._embedded.events;
-       console.log(events)
+    
+        console.log(response)
+        // Capture the part of the API results that contain the values we're looking for 
+        var events = response._embedded.events;
+        console.log(events)
 
+        //Loop through these results, capture the values of interest, declare an object w/ those values and pass that object into the Firebase push method (sending them up to the DB)
         for (var i = 0; i < events.length; i++) {
 
-            var eventName = events[i].name;
-            var venue = events[i]._embedded.venues[0].name
-            var eventDate = events[i].dates.start.localDate;
-            var ticketsStart = events[i].priceRanges[0].min;   
+            // Capture values from the ajax call
+            var name = events[i].name;
+            var venue = events[i]._embedded.venues[0].name;
+            var date = events[i].dates.start.localDate;
             var buyTickets = events[i].url; 
             var image = events[i].images[1].url; 
+            var lat = events[i]._embedded.venues[0].location.latitude;
+            var long = events[i]._embedded.venues[0].location.longitude; 
 
-            console.log(eventName);
+            // This info doesn't exit for all returns. Capture it, if the current return does. Error will throw otherwise. 
+            if (events[i].priceRanges) {
+                var ticketsStart = events[i].priceRanges[0].min;
+            } else {
+                var ticketsStart = "Ticket prices are not listed."; 
+            }
+
+            // Firebase won't work with undefined values
+            if (events[i].info) {
+                var generalInfo = events[i].info;
+            } else {
+                var generalInfo = "No general information has been specified for this event."
+            }
+
+            // Firebase won't work with undefined values
+            if (events[i].pleaseNote) {
+                var notes = events[i].pleaseNote;
+            } else {
+                var notes = "No notes specified for this event."
+            }
+        
+            // Log em to test
+            console.log(name);
             console.log(venue);
-            console.log(eventDate); 
+            console.log(date); 
             console.log(ticketsStart); 
             console.log(buyTickets); 
             console.log(image); 
+            console.log(generalInfo); 
+            console.log(notes); 
+            console.log("latitude: " + lat + ", Longitude: " + long);
+
+            //Temp. store them in an object
+            var results = {
+                name: name, 
+                venue: venue, 
+                date: date,
+                ticketsStart: ticketsStart, 
+                buyTickets: buyTickets, 
+                image: image, 
+                generalInfo: generalInfo, 
+                notes: notes,
+                lat: lat, 
+                long: long
+            };
+
+            // Pass that object to firebase 
+            database.ref("searchResult-" + searchResultID).push(results); 
 
         }
+
+        // Direct user to the results page 
+        location.assign("index.html")
     
     })     
+
+    
 
 })
 
